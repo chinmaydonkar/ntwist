320e2eaaf90cb58bad682628252f52027f8d214a
chinmaydonkar <chinmaydonkar@gmail.com>
2023-11-25 15:38:53 +0530

    structure

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Navbar} from "./Navbar"
+import {Footer} from "./Footer"
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
